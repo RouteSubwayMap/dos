134c1139e588fdbd10025a697b6203b6ce5dfc57
RouteSubwayMap <qw>
2019-02-02 15:03:21 +0800

    修改编码为GBK

--- statisFileCount1.bat
+++ statisFileCount1.bat
@@ -1,6 +1,6 @@
 @echo off&setlocal enabledelayedexpansion
 rem 统计大文件夹里每个小文件夹中的文件数量
-%1 %0 :|more /t20 >???.txt&start ???.txt&exit
+%1 %0 :|more /t20 >文件.txt&start 文件.txt&exit
 for /f "delims=U" %%a in ('cmd /u /c echo ??') do set "tab=%%a"
 for /d %%a in (*) do (
     set n=0
